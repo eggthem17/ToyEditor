ea9126a4bad669bb9adb7e57c273d19486641986
paxmiler <paxmiler@gmail.com>
2022-10-21 12:31:28 +0900

    Initial Commit

---
+++ ToyEditor/ToyEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToyEditorApp.swift
+//  ToyEditor
+//
+//  Created by Martin.Q on 2022/10/21.
+//
+
+import SwiftUI
+
+@main
+struct ToyEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
